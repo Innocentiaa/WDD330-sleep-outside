14b660a12c9e2878a5ed67916851dc25e60ba0e0
Innocentiaa <eke24009@byui.edu>
2025-09-15 01:44:40 +0100

    bug fixed

--- src/js/product.js
+++ src/js/product.js
@@ -4,16 +4,9 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  // 1. Get existing cart or start with an empty array
   let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-
-  // 2. Add the new product
-  cart.push(product);
-
-  // 3. Save updated cart back to localStorage
-  setLocalStorage("so-cart", cart);
+  setLocalStorage("so-cart", product);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
